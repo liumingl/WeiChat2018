8b6c9b5d991af9cdc051e9890c30849ea58b78b8
Liuming <liuming_cn@qq.com>
2018-12-16 07:29:54 +0800

    20181216-提交GroupVC和InviteVC

---
+++ WeiChat/SecondaryViews/InviteUsersViewController.swift
@@ -0,0 +1,25 @@
+//
+//  InviteUsersViewController.swift
+//  WeiChat
+//
+//  Created by 刘铭 on 2018/12/9.
+//  Copyright © 2018 刘铭. All rights reserved.
+//
+
+import UIKit
+
+class InviteUsersTableViewController: UITableViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    
+  }
+  
+  
+  
+}
+
+extension InviteUsersTableViewController: UITableViewDelegate,UITableViewDataSource {
+  
+}
